e3b45a6b602be9c6680f18439c9cc100bce45ece
jlopezSonFe <129200382+jlopezSonFe@users.noreply.github.com>
2023-05-17 12:14:38 +0200

    Update CircleArea.java

--- CircleArea.java
+++ CircleArea.java
@@ -2,21 +2,9 @@ import java.util.Scanner;
 
 public class CircleArea {
     public static void main(String[] args) {
-    	double radius = 0;
-    	radius = radiusRequest(radius);
-    	double area = calculateArea(radius);
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
+        double area = Math.PI * radius * radius;
         System.out.println("The area of the circle is: " + area);
     }
-    
-    public static double radiusRequest(double radius) {
-        Scanner input = new Scanner(System.in);
-        return radius = input.nextDouble();
-    }
-    
-    public static double calculateArea(double radius) {
-    	double area = Math.PI * radius * radius;
-    	return area;
-    }
-    
 }
-
